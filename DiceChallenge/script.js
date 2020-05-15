31c4bd0a655ece58f53dc127354d1586ac10007f
Emily Littler <emilylittler1@gmail.com>
2020-05-15 13:50:35 +0100

    dice game working as 1 function in the console

--- DiceChallenge/script.js
+++ DiceChallenge/script.js
@@ -1,31 +1,38 @@
-
-
 let totalScore = 0;
-let maxScore = 20;
-
-
-
 /*if 1 - console log you fail
+if 20 - you win
+
 else add result to running total up to max of 20
 else statement if total is 20+ console log u win
-need to define a total score and a max score */
+need to define a total score*/
 
 const diceRoll = () => {
  let diceRollResult = (Math.ceil(Math.random()* 6))
-console.log(diceRollResult);
+console.log(diceRollResult); //check the result
+totalScore+=diceRollResult; //adds the roll to your total
+if (diceRollResult == 1) //if its 1, logs u lose, and resets the total.
+    {
+    console.log(`you lost`);
+    totalScore = 0;//reset total score
+    return totalScore;
+    }  
 
-if (diceRollResult == 1)
-{console.log(`you lost`);}
-else {totalScore+=diceRollResult}
-    if (totalScore >= 20)
-    {console.log(`you Won. Your score is ${totalScore}`)}
-    else 
-        {console.log(`your score is ${totalScore}`)};  
- return totalScore; 
+else if (totalScore >= 20) //checks if your total is more or equal to 20
+    {console.log(`you win, your score is ${totalScore}`); //says u win
+    totalScore = 0;
+    return totalScore; //reset the score and can restart the game
 }
 
-;
-diceRoll();
+else { //the total is less than 20
+    console.log(`your score is ${totalScore}`); //show what the score is
+    return totalScore; //return the score for next roll
+}
+}
+
+
+
+
+
 diceRoll();
 diceRoll();
 diceRoll();
